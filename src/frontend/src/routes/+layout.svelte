c758a7a6ea58b56375da8e9a98f49c8ce0eb0b1c
williamreis <wjsreis@gmail.com>
2025-07-11 01:25:27 -0400

    chore: Ajuste textual

--- src/frontend/src/routes/+layout.svelte
+++ src/frontend/src/routes/+layout.svelte
@@ -82,7 +82,7 @@
           </ul>
         </div>
         <a href="/" class="btn btn-ghost normal-case text-xl font-bold tracking-tight hover:bg-primary/20 transition-all duration-300">
-          <span class="text-white">Documento RAG</span>
+          <span class="text-white">Gestão de Documento | RAG</span>
         </a>
       </div>
       <div class="navbar-center hidden lg:flex">
@@ -114,7 +114,7 @@
               <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
               </svg>
-              Documento
+              Documentos
             </a>
           </li>
         </ul>
